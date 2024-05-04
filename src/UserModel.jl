eba4746e87fdf00d237e08129fd04663b327fafc
mingtaoli <mingtao@xjtu.edu.cn>
2024-05-04 21:03:57 +0800

    数据库查询完善

--- src/UserModel.jl
+++ src/UserModel.jl
@@ -1,7 +1,9 @@
 
 module UserModel
 using LibPQ
+using DataFrames
 const DB_CONFIG = Dict(
+    "type" => "postgresql"
     "host" => "localhost",
     "port" => 5432,
     "dbname" => "ai4e-datacenter-postgres",
@@ -9,17 +11,34 @@ const DB_CONFIG = Dict(
     "password" => "dlgcdxlgjzdsys1234"
 )
 
-#conn_str="host=$(DB_CONFIG["host"]) port=$(DB_CONFIG["port"]) dbname=$(DB_CONFIG["dbname"]) user=$(DB_CONFIG["user"]) password=$(DB_CONFIG["password"])"
 
-#const conn=LibPQ.Connection(conn_str)
+struct DatabaseObject
+    conn::LibPQ.Connection
+    tablename::string
+end
 
 const TableName = "users"
 
+# 应该定义User 类型
+# 然后再定义Database类型
+# 然后定义函数增删改查，用多重分发，十分julian
+#function Insert(databaseobject::Database,user::User)
+#end
 
-#result=LibPQ.execute(conn, "SELECT * from users;")
-#using DataFrames
-#tabledata=DataFrame(result)
-function FindOneByLoginRequest(userloginrequest)
+function get_conn_str()
+    return "host=$(DB_CONFIG["host"]) port=$(DB_CONFIG["port"]) dbname=$(DB_CONFIG["dbname"]) user=$(DB_CONFIG["user"]) password=$(DB_CONFIG["password"])"
+end
+
+function FindOneByUserName(username)
+    conn_str = get_conn_str()
+    conn = LibPQ.Connection(conn_str)
+    try
+        query = "SELECT * FROM $TableName WHERE username = $(username)"
+        result = LibPQ.execute(conn, query)
+        return DataFrame(result)
+    finally
+        close(conn)
+    end
 end
 
 end
