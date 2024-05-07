cebf46c34aa010ea9ec5a6dd6244be84aeb0bfd8
mingtaoli <mingtao@xjtu.edu.cn>
2024-05-07 09:03:18 +0800

    使用更julian的表达

--- src/UserModel.jl
+++ src/UserModel.jl
@@ -1,9 +1,9 @@
-
 module UserModel
-using LibPQ
-using DataFrames
+using LibPQ, DataFrames
+
+# 数据库配置常量
 const DB_CONFIG = Dict(
-    "type" => "postgresql"
+    "type" => "postgresql",
     "host" => "localhost",
     "port" => 5432,
     "dbname" => "ai4e-datacenter-postgres",
@@ -11,30 +11,39 @@ const DB_CONFIG = Dict(
     "password" => "dlgcdxlgjzdsys1234"
 )
 
+abstract type AbstractDataSource end
 
-struct DatabaseObject
+# 定义PostgreSQL数据源
+struct PGSQL <: AbstractDataSource
     conn::LibPQ.Connection
-    tablename::string
+    tablename::String
 end
 
 const TableName = "users"
 
-# 应该定义User 类型
-# 然后再定义Database类型
-# 然后定义函数增删改查，用多重分发，十分julian
-#function Insert(databaseobject::Database,user::User)
-#end
+# 定义用户类型
+struct User
+    username::String
+    # 可以根据需要添加更多字段
+end
 
+# 生成数据库连接字符串
 function get_conn_str()
     return "host=$(DB_CONFIG["host"]) port=$(DB_CONFIG["port"]) dbname=$(DB_CONFIG["dbname"]) user=$(DB_CONFIG["user"]) password=$(DB_CONFIG["password"])"
 end
 
-function FindOneByUserName(username)
+"""
+    根据用户名查询用户信息
+    - username::String: 用户名
+    - datasource::PGSQL: 数据源
+    返回值：DataFrame包含查询结果
+"""
+function find_one_by_username(username::String, datasource::PGSQL)
     conn_str = get_conn_str()
     conn = LibPQ.Connection(conn_str)
     try
-        query = "SELECT * FROM $TableName WHERE username = $(username)"
-        result = LibPQ.execute(conn, query)
+        query = "SELECT * FROM $(datasource.tablename) WHERE username = \$1"
+        result = LibPQ.execute(conn, query, [username])
         return DataFrame(result)
     finally
         close(conn)
